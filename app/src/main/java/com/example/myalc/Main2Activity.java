ed5c6744ee98d0c92b20f0933fd33ab30a01dc7f
Ayomilotunde <51304919+Ayomilotunde@users.noreply.github.com>
2019-07-14 18:35:07 +0100

    update

--- app/src/main/java/com/example/myalc/Main2Activity.java
+++ app/src/main/java/com/example/myalc/Main2Activity.java
@@ -2,13 +2,17 @@ package com.example.myalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class Main2Activity extends AppCompatActivity {
 
     private WebView webView;
+    //private WebView ALCWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,22 @@ public class Main2Activity extends AppCompatActivity {
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://andela.com/alc/");
 
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                Log.d("Ssl Error:",handler.toString() + "error:" +  error);
+                handler.proceed();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
     }
+
+
+
 }
